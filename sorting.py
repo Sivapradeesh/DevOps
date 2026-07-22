551a2eead917a8fb0e89c9ce771e8986c4a1b7a9
Barath2662 <barathvikraman2@gmail.com>
2026-07-22 16:45:52 +0530

    Add sorting feature

---
+++ sorting.py
@@ -0,0 +1,18 @@
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+
+print("Original array:", numbers)
+
+sorted_numbers = bubble_sort(numbers)
+
+print("Sorted array:", sorted_numbers)
